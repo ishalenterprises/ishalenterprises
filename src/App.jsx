a518ea4f5c35fbc3c9ff10d9411eda08927d46e8
Hammad <ansarihamad084@gmail.com>
2025-07-22 00:24:33 +0500

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
-import DevelopersPage from './Pages/DevelopersPage'
-import UsersPage from './Pages/UsersPage'
-import DeveloperDashboard from './Pages/DevelopersDashboard'
-import ForgotPassword from './Components/ForgotPassword'
-import ForgotPasswordUser from './Components/ForgotPasswordUser'
 
 function App() {
 
@@ -15,11 +10,6 @@ function App() {
       <Routes>
         <Route path='/' element = {<Navigate to = "/home" />} />
         <Route path='/home' element = {<HomePage />} />
-        <Route path='/developers-auth' element = {<DevelopersPage />} />
-        <Route path='/users-auth' element = {<UsersPage />} />
-        <Route path='/dashboard' element = {<DeveloperDashboard />} />
-        <Route path='/forgot-developer' element = {<ForgotPassword />} />
-        <Route path='/forgot-user' element = {<ForgotPasswordUser />} />
       </Routes>
     </>
   )
