0c07086d5b9e728a4177b258e8c0afc87c8e6e88
Hammad <ansarihamad084@gmail.com>
2025-07-31 03:30:19 +0500

    Changes made to Civil Service

--- src/Pages/ServicePage/CivilWork.jsx
+++ src/Pages/ServicePage/CivilWork.jsx
@@ -19,7 +19,7 @@ const CivilWork = () => {
             <img className='w-full h-full object-cover' src='/services-page/construction.jpeg' />
             </div>
 
-            <div className='inset-0 z-30 bg-black/80 absolute'></div>
+            <div className='inset-0 z-30 bg-black/70 md:bg-black/80 absolute'></div>
 
             <div className='inset-0 z-40 absolute flex items-center justify-center'>
                 <h1 className='text-center text-2xl sm:text-4xl lg:text-4xl text-bold xl:text-6xl pb-1 text-[rgb(54,137,185)]'>Civil Work</h1>
