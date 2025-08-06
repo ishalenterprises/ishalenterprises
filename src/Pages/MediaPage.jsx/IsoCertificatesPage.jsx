46bafe3e3795a89ade548f85d6ae2edfbb8e72bc
ishalenterprises <procurement@ishalenterprises.com.pk>
2025-08-06 16:22:35 +0500

    Update and rename IsoCertificates.jsx to IsoCertificatesPage.jsx

--- src/Pages/MediaPage.jsx/IsoCertificatesPage.jsx
+++ src/Pages/MediaPage.jsx/IsoCertificatesPage.jsx
@@ -2,19 +2,21 @@ import React, { useEffect } from 'react';
 import PotfolioAndGallery from '../../Components/PotfolioAndGallery';
 import Header from '../../Components/Header';
 import Footer from '../../Components/Footer';
+import IsoCertificates from '../../Components/IsoCertificates';
 
 
-const IsoCertificates = () => {
+const IsoCertificatesPage = () => {
     useEffect(() => {
             window.scrollTo(0, 0);
         }, []);
     return (
         <>
         <Header />
+        <IsoCertificates />
         <Footer />
         
         </>
     )
 }
 
-export default IsoCertificates;
+export default IsoCertificatesPage;
